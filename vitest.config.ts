4b1e0c36e8557e79a345e0f8b3adde322142d15b
qkal <kal@complexia.org>
2026-03-28 22:48:11 +0100

    chore: project scaffold

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte({ hot: !process.env.VITEST })],
+  test: {
+    environment: 'jsdom',
+    include: ['tests/**/*.test.ts', 'tests/**/*.test.svelte.ts'],
+  },
+});
